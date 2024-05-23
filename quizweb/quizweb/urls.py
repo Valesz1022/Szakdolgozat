b7ac9a9abadc22b6f15628574f51be9a03a35c26
Valentin <145005466+Valesz1022@users.noreply.github.com>
2024-05-23 21:15:00 +0200

    register

--- quizweb/quizweb/urls.py
+++ quizweb/quizweb/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from django.views.generic import RedirectView
+from django.views.static import serve
+from django.conf import settings
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('register.urls')),
 ]
